32e4a0108df05ada2de5a9de9f3bcea115689852
Yaohua Xiong <xiongyaohua@gmail.com>
2023-10-13 21:19:23 +0800

    silence unused parameters

--- gdext/project/World.gd
+++ gdext/project/World.gd
@@ -46,7 +46,7 @@ func _ready() -> void:
 	_pathes.add_child(pathflow)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func _draw() -> void:
